dbbf73e26c5d3b0f66b995178cb490d2c01df224
AHmed-ELsewedy <150848364+A7medelswedy@users.noreply.github.com>
2025-11-09 20:01:50 +0200

    Update PostReadDto.cs
    Changed CreatedAt property type from DateTime to string.

--- Dtos/PostDTO/PostReadDto.cs
+++ Dtos/PostDTO/PostReadDto.cs
@@ -1,11 +1,12 @@
-﻿namespace Social_Media_Web_API.Dtos.PostDTO
+namespace Social_Media_Web_API.Dtos.PostDTO
 {
     public class PostReadDto
     {
         public int Id { get; set; }
         public string Content { get; set; }
-        public string UserName { get; set; } // ناخدها من User
-        public DateTime CreatedAt { get; set; }
+        public string UserName { get; set; }
+        
+        // string not datetime
+        public string CreatedAt { get; set; }
     }
-
 }
